c23a5678445fd291c413c73097881afb8937d70d
Kiran Kumar <mandla.kiran5143@gmail.com>
2026-02-05 14:01:37 +0530

    updated

---
+++ vendor/signifly/laravel-shopify/src/Exceptions/Handler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Signifly\Shopify\Exceptions;
+
+use Illuminate\Http\Client\Response;
+use Illuminate\Support\Arr;
+
+class Handler implements ErrorHandlerInterface
+{
+    public function handle(Response $response)
+    {
+        if ($response->successful()) {
+            return;
+        }
+
+        if ($response->status() === 429) {
+            throw new TooManyRequestsException($response);
+        }
+
+        if ($response->status() === 422) {
+            throw new ValidationException(Arr::wrap($response->json('errors', [])));
+        }
+
+        if ($response->status() === 404) {
+            throw new NotFoundException();
+        }
+
+        $response->throw();
+    }
+}
